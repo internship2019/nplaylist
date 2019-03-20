6237f8641fa4fa3dbec016456f401c138ef081ca
Max Podlesnov <maksim.podlesnov@mail.ru>
2019-03-20 13:13:52 +0200

    -BasePlaylist class added
    -BaseItem class added
    
    Cat class returned

--- demo/NPlaylist.ReadingPlaylists/Program.cs
+++ demo/NPlaylist.ReadingPlaylists/Program.cs
@@ -6,7 +6,7 @@ namespace NPlaylist.ReadingPlaylists
     {
         private static void Main(string[] args)
         {
-           
+            Console.WriteLine(new Cat().Meow());
         }
     }
 }
---
+++ src/NPlaylist/Cat.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPlaylist
+{
+    
+        public class Cat
+        {
+            public string Meow()
+            {
+                return "Meow!";
+            }
+        }
+    
+}
